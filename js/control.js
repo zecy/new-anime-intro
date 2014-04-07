29c08c4ffdde87f493fc67f86545f26e645a4469
zecy <angelszc163@gmail.com>
2014-04-07 19:37:22 +0800

    rebuild all the main functions includes
    buildAnimeList
    buildCountedList
    buildWaitToCountList
    
    and so on.

--- js/control.js
+++ js/control.js
@@ -352,7 +352,7 @@ function animeBox($scope) {
     };
 
     $scope.animeNumberInOnePage = function() {
-        $scope.animeDataBase = buildAnimeList(dataBase, [sN, eN], 1)
+        $scope.animeDataBase = buildAnimeList(shownAnimeData, [sN, eN], 1)
         $scope.countedStaff = meaningfulData()
     };
 }
