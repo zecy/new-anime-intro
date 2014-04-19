7de5f726608ee936f9f7f920a184bd09d4c595a8
zecy <angelszc163@gmail.com>
2014-04-19 14:39:36 +0800

    add the style
    * auto drop down pane

--- js/control.js
+++ js/control.js
@@ -11,20 +11,20 @@
 function arrCompact(arr, style) {
 
     /*
-    *
-    *  compact the items
-    *
-    *  style = 1: positon1/positon2... : member
-    *
-    *  or
-    *
-    *  style = 2: position: member1、member2...
-    *
-    *  or
-    *
-    *  style = 3: both of these.
-    *
-    * */
+     *
+     *  compact the items
+     *
+     *  style = 1: positon1/positon2... : member
+     *
+     *  or
+     *
+     *  style = 2: position: member1、member2...
+     *
+     *  or
+     *
+     *  style = 3: both of these.
+     *
+     * */
 
     var style = arguments[1] ? arguments[1] : 3;
 
@@ -37,13 +37,13 @@ function arrCompact(arr, style) {
             if ( ( style == 1 || style == 3 ) && arr[i][1] === arr[j][1] ) {
 
                 /*多职位同人
-                arr[i - 1][0] = arr[i - 1].shift() + "/" + arr[i][0];
-                arr[i - 1][1] = arr[i][1];
-                arr[i - 1][2] = arr[i][2]
-                arr.splice(i, 1);
-                */
+                 arr[i - 1][0] = arr[i - 1].shift() + "/" + arr[i][0];
+                 arr[i - 1][1] = arr[i][1];
+                 arr[i - 1][2] = arr[i][2]
+                 arr.splice(i, 1);
+                 */
                 arr[i][0] = arr[i][0] + "/" + arr[j][0];
-                arr.splice(j,1);
+                arr.splice(j, 1);
                 j -= 1;
                 arrLength -= 1;
                 continue
@@ -53,13 +53,13 @@ function arrCompact(arr, style) {
             if ( ( style == 2 || style == 3 ) && arr[i][0] === arr[j][0] ) {
 
                 /*多人同职位
-                arr[i - 1][0] = arr[i][0];
-                arr[i - 1][1] = arr[i - 1][1] + "、" + arr[i][1];
-                arr.splice(i, 1);
+                 arr[i - 1][0] = arr[i][0];
+                 arr[i - 1][1] = arr[i - 1][1] + "、" + arr[i][1];
+                 arr.splice(i, 1);
                  */
 
                 arr[i][1] = arr[i][1] + "、" + arr[j][1];
-                arr.splice(j,1);
+                arr.splice(j, 1);
 
                 arrLength -= 1;
                 j -= 1;
@@ -80,8 +80,8 @@ function formatDataBase(data) {
 
     for ( var j = 0; j < databaseLength; j++ ) {
         var obj = database[j];
-        obj.staff = arrCompact(obj.staff,3);
-        obj.cast = arrCompact(obj.cast,3);
+        obj.staff = arrCompact(obj.staff, 3);
+        obj.cast = arrCompact(obj.cast, 3);
     }
 
     return database
@@ -89,26 +89,26 @@ function formatDataBase(data) {
 
 function buildAnimeList(data, indexlist, conswitch) {
     /*
-    *
-    * Import a database
-    *
-    * for the MAIN ANIME SHOWN
-    *
-    * can build the anime list by a indexlist,
-    *
-    * or between the start number and end number
-    *
-    * when the conswitch is on( 1 by default ), and the indexlist just have 2 elements
-    *
-    * [24, 37]
-    *
-    * then the animes between 24 and 37 would output
-    *
-    * when the switch is off( -1 ), just 24 and 37 will output
-    *
-    * NOTE: the indexlist have a default value = [0, data.length]
-    *
-    * */
+     *
+     * Import a database
+     *
+     * for the MAIN ANIME SHOWN
+     *
+     * can build the anime list by a indexlist,
+     *
+     * or between the start number and end number
+     *
+     * when the conswitch is on( 1 by default ), and the indexlist just have 2 elements
+     *
+     * [24, 37]
+     *
+     * then the animes between 24 and 37 would output
+     *
+     * when the switch is off( -1 ), just 24 and 37 will output
+     *
+     * NOTE: the indexlist have a default value = [0, data.length]
+     *
+     * */
 
     var outputDataBase = [];
 
@@ -121,7 +121,7 @@ function buildAnimeList(data, indexlist, conswitch) {
     var sn = index[0];
     var en = index[indexLength - 1];
 
-    if( cs == 1 ) {
+    if ( cs == 1 ) {
 
         for ( var i = sn; i < en; i++ ) {
             var anime = data[i];
@@ -175,14 +175,14 @@ function buildWaitToCountList(dataItem, indexlist) {
 
     var index = arguments[1] ? arguments[1] : [0, dataItem.length];
     var sn = index[0];
-    var en = index[index.length-1];
+    var en = index[index.length - 1];
 
-    var onairList       = [];
-    var genreList       = [];
-    var origintypeList  = [];
-    var sequelList      = [];
-    var staffList       = [];
-    var castList        = [];
+    var onairList = [];
+    var genreList = [];
+    var origintypeList = [];
+    var sequelList = [];
+    var staffList = [];
+    var castList = [];
 
     function staffAndCastConvert(arr, index) {
 
@@ -190,7 +190,7 @@ function buildWaitToCountList(dataItem, indexlist) {
 
         var arrLength = arr.length;
 
-        for ( var i = 0; i < arrLength;  i++ ) {
+        for ( var i = 0; i < arrLength; i++ ) {
             var obj = arr[i];
             var objList = [];
             var waitToCountItem = {};
@@ -224,38 +224,38 @@ function buildWaitToCountList(dataItem, indexlist) {
     for ( var i = sn; i < en; i++ ) {
         var anime = dataItem[i];
 
-        var onair          = anime.info.onair[0].split(" ");
-        var genre          = anime.info.genre[0].split(",");
-        var origintype     = [];
-        origintype[0]      = anime.info.origintype[0];
-        var sequel         = [];
-        sequel[0]          = anime.info.sequel[0];
-        var staff          = arrCompact(anime.staff,1);
-        var cast           = arrCompact(anime.cast,1);
+        var onair = anime.info.onair[0].split(" ");
+        var genre = anime.info.genre[0].split(",");
+        var origintype = [];
+        origintype[0] = anime.info.origintype[0];
+        var sequel = [];
+        sequel[0] = anime.info.sequel[0];
+        var staff = arrCompact(anime.staff, 1);
+        var cast = arrCompact(anime.cast, 1);
 
-        onair       = covert(onair, 'onair', i);
-        genre       = covert(genre, 'genre', i);
-        origintype  = covert(origintype, 'origintype', i);
-        sequel      = covert(sequel, 'sequel', i)
-        staff       = staffAndCastConvert(staff, i);
-        cast        = staffAndCastConvert(cast, i);
+        onair = covert(onair, 'onair', i);
+        genre = covert(genre, 'genre', i);
+        origintype = covert(origintype, 'origintype', i);
+        sequel = covert(sequel, 'sequel', i)
+        staff = staffAndCastConvert(staff, i);
+        cast = staffAndCastConvert(cast, i);
 
-        onairList       = onairList.concat(onair);
-        genreList       = genreList.concat(genre);
-        origintypeList  = origintypeList.concat(origintype);
-        sequelList      = sequelList.concat(sequel);
-        staffList       = staffList.concat(staff);
-        castList        = castList.concat(cast);
+        onairList = onairList.concat(onair);
+        genreList = genreList.concat(genre);
+        origintypeList = origintypeList.concat(origintype);
+        sequelList = sequelList.concat(sequel);
+        staffList = staffList.concat(staff);
+        castList = castList.concat(cast);
 
     }
 
     return {
-        onair      : onairList,
-        genre      : genreList,
-        origintype : origintypeList,
-        sequel     : sequelList,
-        staff      : staffList,
-        cast       : castList
+        onair     : onairList,
+        genre     : genreList,
+        origintype: origintypeList,
+        sequel    : sequelList,
+        staff     : staffList,
+        cast      : castList
     };
 }
 
@@ -287,7 +287,7 @@ function meaningfulData(obj, number) {
 
     for ( var i in obj ) {
 //        console.log(obj[i]);
-        if( obj[i].counted >= number) {
+        if ( obj[i].counted >= number ) {
             var item = {};
             item['name'] = i;
             item['number'] = obj[i].counted;
@@ -295,7 +295,7 @@ function meaningfulData(obj, number) {
         }
     }
 //    console.log(meaningList);
-    meaningList.sort(function(a, b) {
+    meaningList.sort(function (a, b) {
         return b.number - a.number
     });
 
@@ -304,10 +304,10 @@ function meaningfulData(obj, number) {
 }
 
 /*------------------------------------------------------------
-*
-*  THE FILTERS
-*
-* ------------------------------------------------------------*/
+ *
+ *  THE FILTERS
+ *
+ * ------------------------------------------------------------*/
 
 var animeIntro = angular.module('AnimeIntro', []);
 
@@ -335,7 +335,7 @@ animeIntro.filter('weekDays', function () {
         var inputLength = input.length;
         for ( var i = 0; i < inputLength; i++ ) {
             var item = input[i];
-            switch (item.name) {
+            switch ( item.name ) {
                 case '星期一':
                     output[0] = item;
                     break;
@@ -365,16 +365,16 @@ animeIntro.filter('weekDays', function () {
 
 
 /*------------------------------------------------------------
-*
-*                   MAIN FUNCTION
-*
-* ------------------------------------------------------------*/
+ *
+ *                   MAIN FUNCTION
+ *
+ * ------------------------------------------------------------*/
 
 /*
-*
-*  SHOW ANIME
-*
-* */
+ *
+ *  SHOW ANIME
+ *
+ * */
 
 // format the staff and cast list
 
@@ -382,26 +382,27 @@ var shownAnimeData = formatDataBase(animeDataBase);
 
 var waitToCountList = buildWaitToCountList(animeDataBase);
 
-var countedOnair      = buildCountedList(waitToCountList.onair);
-var countedGenre      = buildCountedList(waitToCountList.genre);
+var countedOnair = buildCountedList(waitToCountList.onair);
+var countedGenre = buildCountedList(waitToCountList.genre);
 var countedOrigintype = buildCountedList(waitToCountList.origintype);
-var countedSequel     = buildCountedList(waitToCountList.sequel);
-var countedStaff      = buildCountedList(waitToCountList.staff);
-var countedCast       = buildCountedList(waitToCountList.cast);
+var countedSequel = buildCountedList(waitToCountList.sequel);
+var countedStaff = buildCountedList(waitToCountList.staff);
+var countedCast = buildCountedList(waitToCountList.cast);
 
 function animeBox($scope) {
 
-    $scope.animeDataBase     = buildAnimeList(shownAnimeData);
-    $scope.animeNumber       = animeDataBase.length;
+    $scope.animeDataBase      = buildAnimeList(shownAnimeData);
+    $scope.animeNumber        = animeDataBase.length;
+    $scope.animeCurrentNumber = animeDataBase.length;
 
-    $scope.countedOnair      = meaningfulData(countedOnair, 1);
-    $scope.countedGenre      = meaningfulData(countedGenre, 1);
-    $scope.countedOrigintype = meaningfulData(countedOrigintype, 1);
-    $scope.countedSequel     = meaningfulData(countedSequel, 1);
-    $scope.countedStaff      = meaningfulData(countedStaff, 2);
-    $scope.countedCast       = meaningfulData(countedCast, 3);
+    $scope.countedOnair       = meaningfulData(countedOnair, 1);
+    $scope.countedGenre       = meaningfulData(countedGenre, 1);
+    $scope.countedOrigintype  = meaningfulData(countedOrigintype, 1);
+    $scope.countedSequel      = meaningfulData(countedSequel, 1);
+    $scope.countedStaff       = meaningfulData(countedStaff, 2);
+    $scope.countedCast        = meaningfulData(countedCast, 3);
 
-    $scope.currentName       = "";
+    $scope.currentName = "";
 
     $scope.searchProperty = function (str, type) {
 
@@ -417,7 +418,7 @@ function animeBox($scope) {
 
         var indexList = [];
 
-        switch (type) {
+        switch ( type ) {
             case 'onair' :
                 indexList = countedOnair[str].list;
                 break;
@@ -459,13 +460,13 @@ function animeBox($scope) {
         }
     };
 
-    $scope.unFilt = function() {
+    $scope.unFilt = function () {
 
         /*
-        *
-        * The GoBack function for the searchProperty(). Once click this , the member list will come back.
-        *
-        * */
+         *
+         * The GoBack function for the searchProperty(). Once click this , the member list will come back.
+         *
+         * */
 
         var sN = $scope.pageStart ? $scope.pageStart - 1 : 0;
         var eN = $scope.pageEnd ? $scope.pageEnd : shownAnimeData.length;
@@ -474,42 +475,45 @@ function animeBox($scope) {
 
     };
 
-    $scope.animeNumberInOnePage = function() {
+    $scope.animeNumberInOnePage = function () {
 
         var sN = $scope.pageStart ? $scope.pageStart - 1 : 0;
-        var eN = $scope.pageEnd ? $scope.pageEnd: shownAnimeData.length;
+        var eN = $scope.pageEnd ? $scope.pageEnd : shownAnimeData.length;
 
         var animeDataBase = buildAnimeList(shownAnimeData, [sN, eN], 1);
 
-        $scope.animeDataBase = animeDataBase;
+        $scope.animeDataBase      = animeDataBase;
+        $scope.animeCurrentNumber = animeDataBase.length;
 
-        /*
-        var waitToCountList = buildWaitToCountList(animeDataBase);
+            /*
+             var waitToCountList = buildWaitToCountList(animeDataBase);
 
-        var countedOnair      = buildCountedList(waitToCountList.onair);
-        var countedGenre      = buildCountedList(waitToCountList.genre);
-        var countedOrigintype = buildCountedList(waitToCountList.origintype);
-        var countedSequel     = buildCountedList(waitToCountList.sequel);
-        var countedStaff      = buildCountedList(waitToCountList.staff);
-        var countedCast       = buildCountedList(waitToCountList.cast);
+             var countedOnair      = buildCountedList(waitToCountList.onair);
+             var countedGenre      = buildCountedList(waitToCountList.genre);
+             var countedOrigintype = buildCountedList(waitToCountList.origintype);
+             var countedSequel     = buildCountedList(waitToCountList.sequel);
+             var countedStaff      = buildCountedList(waitToCountList.staff);
+             var countedCast       = buildCountedList(waitToCountList.cast);
 
-        $scope.countedOnair      = meaningfulData(countedOnair, 1);
-        $scope.countedGenre      = meaningfulData(countedGenre, 1);
-        $scope.countedOrigintype = meaningfulData(countedOrigintype, 1);
-        $scope.countedSequel     = meaningfulData(countedSequel, 1);
-        $scope.countedStaff      = meaningfulData(countedStaff, 2);
-        $scope.countedCast       = meaningfulData(countedCast, 3);
-        */
+             $scope.countedOnair      = meaningfulData(countedOnair, 1);
+             $scope.countedGenre      = meaningfulData(countedGenre, 1);
+             $scope.countedOrigintype = meaningfulData(countedOrigintype, 1);
+             $scope.countedSequel     = meaningfulData(countedSequel, 1);
+             $scope.countedStaff      = meaningfulData(countedStaff, 2);
+             $scope.countedCast       = meaningfulData(countedCast, 3);
+             */
 
         $scope.currentName = "";
 
         $scope.pageStart   = "";
         $scope.pageEnd     = "";
+
+        console.log(sN);
     };
 
     $scope.selector = 'pages';
 
-    $scope.tabChange = function(selector) {
+    $scope.tabChange = function (selector) {
         $scope.selector = selector;
     }
 }
